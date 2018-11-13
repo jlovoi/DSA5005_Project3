35ee9ca9cdd8915b227f87e97fd28da701868f84
Joseph Lovoi <joelovoi@gmail.com>
2018-11-13 17:37:35 -0600

    clang errors are fun

--- Project3_Joseph_Lovoi/EdgeList.cpp
+++ Project3_Joseph_Lovoi/EdgeList.cpp
@@ -13,29 +13,51 @@
 using namespace std;
 
 class EdgeList {
-    ostream& operator << (EdgeList& printThis);
+    //ostream& operator << (EdgeList& printThis);
 protected:
     int numNodes;
     int numEdges;
     int* v1;
     int* v2;
+    int index = 0;
 public:
     EdgeList();
     EdgeList(int n, int m);
-    ~EdgeList();
+    void createEdge(int x, int y);
+    //~EdgeList();
     bool isAnEdge(int a, int b);
-    vector<int> listNeighbors(int a);
-    int* DFS(int start);
-    int* BFS(int start);
+    //vector<int> listNeighbors(int a);
+    //int* DFS(int start);
+    //int* BFS(int start);
 };
 
 EdgeList::EdgeList() {
     numNodes = 0;
     numEdges = 0;
+    v1 = new int[1];
+    v2 = new int[1];
 }
 
 EdgeList::EdgeList(int n, int m) {
     numNodes = n;
     numEdges = m;
+    v1 = new int[m];
+    v2 = new int[m];
 }
 
+void EdgeList::createEdge(int x, int y) {
+    v1[index] = x;
+    v2[index] = y;
+    index++;
+}
+
+bool EdgeList::isAnEdge(int a, int b) {
+    int curr = 0;
+    bool ret = false;
+    while (ret != true || curr < sizeof(v1)) {
+        if ((v1[curr] == a && v2[curr] == b) || (v1[curr] == b && v2[curr] == a)) {
+            ret = true;
+        }
+    }
+    return ret;
+}
--- Project3_Joseph_Lovoi/main.cpp
+++ Project3_Joseph_Lovoi/main.cpp
@@ -6,10 +6,20 @@
 //  Copyright © 2018 Joseph Lovoi. All rights reserved.
 //
 
+#include "EdgeList.cpp"
+
 #include <iostream>
 
-int main(int argc, const char * argv[]) {
-    // insert code here...
-    std::cout << "Hello, World!\n";
-    return 0;
+int main() {
+//    EdgeList* edgelist = new EdgeList(5, 7);
+//    (*edgelist).createEdge(1, 5);
+//    (*edgelist).createEdge(1, 4);
+//    (*edgelist).createEdge(1, 3);
+//    (*edgelist).createEdge(2, 5);
+//    (*edgelist).createEdge(3, 3);
+//    (*edgelist).createEdge(4, 3);
+//    (*edgelist).createEdge(4, 5);
+//    cout << (*edgelist).isAnEdge(1, 5) << endl;
+//    cout << (*edgelist).isAnEdge(5, 1) << endl;
+//    cout << (*edgelist).isAnEdge(1, 2) << endl;
 }
