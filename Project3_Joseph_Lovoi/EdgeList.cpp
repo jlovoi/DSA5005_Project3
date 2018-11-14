3a87c349ee68e1330f04cdbdee97fdc248ebbdc0
Joseph Lovoi <joelovoi@gmail.com>
2018-11-13 18:46:36 -0600

    Fixed Clang Error, implemented createEdge, isAnEdge, listNeighbors

--- Project3_Joseph_Lovoi/EdgeList.cpp
+++ Project3_Joseph_Lovoi/EdgeList.cpp
@@ -26,7 +26,7 @@ public:
     void createEdge(int x, int y);
     //~EdgeList();
     bool isAnEdge(int a, int b);
-    //vector<int> listNeighbors(int a);
+    vector<int> listNeighbors(int a);
     //int* DFS(int start);
     //int* BFS(int start);
 };
@@ -54,10 +54,42 @@ void EdgeList::createEdge(int x, int y) {
 bool EdgeList::isAnEdge(int a, int b) {
     int curr = 0;
     bool ret = false;
-    while (ret != true || curr < sizeof(v1)) {
+    while (curr < sizeof(v1)) {
         if ((v1[curr] == a && v2[curr] == b) || (v1[curr] == b && v2[curr] == a)) {
             ret = true;
         }
+        curr++;
     }
     return ret;
+}
+
+vector<int> EdgeList::listNeighbors(int a) {
+    int curr = 0;
+    vector<int>* ret = new vector<int>();
+    while (curr < sizeof(v1)) {
+        if (isAnEdge(a, curr)) {
+            ret->push_back(curr);
+        }
+        curr++;
+    }
+    return (*ret);
+}
+
+int main() {
+    EdgeList* edgelist = new EdgeList(5, 7);
+    cout << "hello" << endl;
+    (*edgelist).createEdge(1, 5);
+    (*edgelist).createEdge(1, 4);
+    (*edgelist).createEdge(1, 3);
+    (*edgelist).createEdge(2, 5);
+    (*edgelist).createEdge(3, 3);
+    (*edgelist).createEdge(4, 3);
+    (*edgelist).createEdge(4, 5);
+    cout << (*edgelist).isAnEdge(1, 5) << endl;
+    cout << (*edgelist).isAnEdge(5, 1) << endl;
+    cout << (*edgelist).isAnEdge(1, 2) << endl;
+    vector<int> one = (*edgelist).listNeighbors(1);
+    for (auto it = one.begin(); it != one.end(); it++) {
+        cout << *it << endl;
+    }
 }
