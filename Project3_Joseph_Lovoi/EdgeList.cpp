98045528c3521a052e8b576049019e5bd84e850b
Joseph Lovoi <joelovoi@gmail.com>
2018-11-16 21:44:50 -0600

    DFS implemented

--- Project3_Joseph_Lovoi/EdgeList.cpp
+++ Project3_Joseph_Lovoi/EdgeList.cpp
@@ -17,9 +17,12 @@ class EdgeList {
 protected:
     int numNodes;
     int numEdges;
+    int* dfs;
+    int* bfs;
     int* v1;
     int* v2;
     int index = 0;
+    int count = 0;
 public:
     EdgeList();
     EdgeList(int n, int m);
@@ -27,7 +30,8 @@ public:
     //~EdgeList();
     bool isAnEdge(int a, int b);
     vector<int> listNeighbors(int a);
-    //int* DFS(int start);
+    int* DFS(int start);
+    void DFSRecur(int x, bool* visited);
     //int* BFS(int start);
 };
 
@@ -43,6 +47,8 @@ EdgeList::EdgeList(int n, int m) {
     numEdges = m;
     v1 = new int[m];
     v2 = new int[m];
+    dfs = new int[numNodes-1];
+    bfs = new int[numNodes-1];
 }
 
 void EdgeList::createEdge(int x, int y) {
@@ -64,7 +70,7 @@ bool EdgeList::isAnEdge(int a, int b) {
 }
 
 vector<int> EdgeList::listNeighbors(int a) {
-    int curr = 0;
+    int curr = 1;
     vector<int>* ret = new vector<int>();
     while (curr < sizeof(v1)) {
         if (isAnEdge(a, curr)) {
@@ -75,9 +81,38 @@ vector<int> EdgeList::listNeighbors(int a) {
     return (*ret);
 }
 
+int* EdgeList::DFS(int start) {
+    // Keeps track of which nodes have been visited
+    bool* visited = new bool[numNodes];
+    for (int i = 0; i < (numNodes); i++) {
+        visited[i] = false;
+    }
+    
+    // Start recurrence sequence
+    DFSRecur(start, visited);
+    return dfs;
+}
+
+void EdgeList::DFSRecur(int x, bool *visited) {
+    // This node, x, has now been visited
+    cout << "ISSA " << x << endl;
+    visited[x] = true;
+    dfs[count] = x;
+    cout << "added " << x << " at " << count << endl;
+    count++;
+    
+    for (auto it = listNeighbors(x).begin(); it != listNeighbors(x).end(); it++) {
+        if (*it < 0 || *it > numNodes) {
+            break;
+        }
+        if (!visited[*it] && *it != 0) {
+            DFSRecur(*it, visited);
+        }
+    }
+}
+
 int main() {
     EdgeList* edgelist = new EdgeList(5, 7);
-    cout << "hello" << endl;
     (*edgelist).createEdge(1, 5);
     (*edgelist).createEdge(1, 4);
     (*edgelist).createEdge(1, 3);
@@ -85,11 +120,15 @@ int main() {
     (*edgelist).createEdge(3, 3);
     (*edgelist).createEdge(4, 3);
     (*edgelist).createEdge(4, 5);
-    cout << (*edgelist).isAnEdge(1, 5) << endl;
-    cout << (*edgelist).isAnEdge(5, 1) << endl;
-    cout << (*edgelist).isAnEdge(1, 2) << endl;
-    vector<int> one = (*edgelist).listNeighbors(1);
+//    cout << (*edgelist).isAnEdge(1, 5) << endl;
+//    cout << (*edgelist).isAnEdge(5, 1) << endl;
+//    cout << (*edgelist).isAnEdge(1, 2) << endl;
+    vector<int> one = (*edgelist).listNeighbors(2);
     for (auto it = one.begin(); it != one.end(); it++) {
         cout << *it << endl;
+    }
+    int* arr = (*edgelist).DFS(5);
+    for (int i = 0; i < 5; i++) {
+        cout << arr[i] << endl;
     }
 }
